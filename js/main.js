5f778c2c6409caa2bb5f13ca2e5cf3d670f6c3b0
m1ghty07 <dulchenko_s@ukr.net>
2021-11-22 21:37:33 +0200

    project completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const tabs = (tabsSelector, tabsContentSelector) => {
+  const tabs = document.querySelectorAll(tabsSelector),
+    tabsContent = document.querySelectorAll(tabsContentSelector);
+
+
+  function hideContent() {
+    tabsContent.forEach(item => {
+      item.style.display = 'none';
+    });
+  }
+
+  function displayContent(i = 0) {
+    tabsContent[i].style.display = 'block';
+  }
+
+  hideContent();
+  displayContent();
+
+  tabs.forEach((item, i) => {
+    item.addEventListener('click', () => {
+      hideContent();
+      displayContent(i);
+    });
+  });
+};
+
+tabs('.tabs__item', '.tabs__content-item');
